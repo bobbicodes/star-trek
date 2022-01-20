126592d255e1383d5ae517c075efc41614666c0c
Bobby Towers <porkostomus@mail.com>
2022-01-20 15:29:21 -0800

    cleanup

--- trek.clj
+++ trek.clj
@@ -5,16 +5,14 @@
 (defn fetch-ep [s e]
   (slurp (str "https://www.springfieldspringfield.co.uk/view_episode_scripts.php?tv-show=star-trek-discovery-2017&episode=s" s "e" e)))
 
-(doseq [s ["04"]
-        e ["01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12" "13" "14" "15"]]
-  (spit (str "disco-s" s "e" e) (fetch-ep s e)))
-
-["s01e90" "s03e00"]
-
-(spit "test" (fetch-ep "01" "01"))
+(comment
+  (doseq [s ["04"]
+          e ["01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12" "13" "14" "15"]]
+    (spit (str "resources/disco/disco-s" s "e" e) (fetch-ep s e)))
+  )
 
 (defn disco-seq [s e]
-  (->  (str "disco-s" s "e" e)
+  (->  (str "resources/disco/disco-s" s "e" e)
        slurp
        (str/split #"<div class=\"scrolling-script-container\">")
        last
@@ -22,8 +20,10 @@
        first
        (str/split #"<br>")))
 
-(doseq [line (disco-seq "02" "01")]
-  (prn line))
+(comment
+  (doseq [line (disco-seq "02" "01")]
+    (prn line))
+  )
 
 (defn episode [series ep]
   (get-in (json/parse-string (slurp "resources/all_scripts_raw.json"))
@@ -36,4 +36,6 @@
    (conj (re-seq #"[A-Z]+:" (episode series ep)) nil)
    (str/split (episode series ep) #"[A-Z]+:")))
 
-(script-seq "TOS" "episode 0")
+(comment
+  (script-seq "TOS" "episode 0")
+  )
