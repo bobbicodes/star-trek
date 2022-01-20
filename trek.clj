b2f50aa3193b96ffe9bd5a16a2741d5bfa05c307
Bobby Towers <porkostomus@mail.com>
2022-01-20 14:54:36 -0800

    scrape disco

--- trek.clj
+++ trek.clj
@@ -1,6 +1,31 @@
 (ns trek
   (:require [cheshire.core :as json]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.data.xml :as xml]))
+
+(defn fetch-ep [s e]
+  (slurp (str "https://www.springfieldspringfield.co.uk/view_episode_scripts.php?tv-show=star-trek-discovery-2017&episode=s" s "e" e)))
+
+(doseq [s ["04"]
+        e ["01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12" "13" "14" "15"]]
+  (spit (str "disco-s" s "e" e) (fetch-ep s e)))
+
+["s01e90" "s03e00"]
+
+(spit "test" (fetch-ep "01" "01"))
+
+
+(xml/parse-str 
+ (-> (slurp  "disco-s01e01")
+     (str/escape {\& "&amp;"})
+     (str/replace "<script async src=" " <script async=\"async\" src=")))
+
+(defn disco-seq [e s]
+  (str/split (last (str/split (slurp  (str "disco-s" s "e" e))
+                              #"<div class=\"scrolling-script-container\">"))
+             #"<br>"))
+
+(disco-seq "01" "02")
 
 (defn episode [series ep]
   (get-in (json/parse-string (slurp "resources/all_scripts_raw.json"))
