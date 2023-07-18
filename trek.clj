2bc23e56f80cc988a4db6e432c7a4d184ea52a36
Bobbi Towers <btowers793@gmail.com>
2023-07-18 14:39:34 -0700

    found a forum with SNW transcripts

--- trek.clj
+++ trek.clj
@@ -8,6 +8,24 @@
 (defn fetch-picard [s e]
   (slurp (str "https://www.springfieldspringfield.co.uk/view_episode_scripts.php?tv-show=star-trek-picard-2020&episode=s" s "e" e)))
 
+(def snw
+  ;; https://transcripts.foreverdreaming.org/viewtopic.php?t= ...
+  {:s01e01 "72376"
+   :s01e02 "72959"
+   :s01e03 "73149"
+   :s01e04 "73773"
+   :s01e05 "73827"
+   :s01e06 "74201"
+   :s01e07 "74648"
+   :s01e08 "75383"
+   :s01e09 "75964"
+   :s01e10 "76357"
+   :s02e01 "116179"
+   :s02e02 "116968"
+   :s02e03 "118367"
+   :s02e04 "119628"
+   :s02e05 "120667"})
+
 (defn episode-seq 
   "Extracts the <div> containing the episode script
    and outputs a sequence of lines."
